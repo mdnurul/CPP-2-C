eea713d00320d870c44d1823f1c4fa37abe74015
Md Nurul Hassan <nurul@aitlindia.com>
2019-01-21 21:19:36 +0530

    c++ template as generic method using #define

--- template/c/src/main.c
+++ template/c/src/main.c
@@ -1,6 +1,7 @@
 
 
 #include <stdio.h>
+#include "checkMaxGeneric.h"
 
 /*
 template <typename T>
@@ -21,6 +22,9 @@ T checkMax (T a, T b) {
 #define checkMaxINT(a,b)  ({int _a = (a), _b = (b); _a > _b ? _a : _b; })
 
 
+DECLARE_CHECK_MAX(int)
+IMPLEMENT_CHECK_MAX(int)
+
 int main()
 {
    // printf() displays the string inside quotation
@@ -28,12 +32,30 @@ int main()
    int i=3, j=7;
    float l=3.5, m=7.6;
    long o=300000, p=700000;
+   int b = 4;
    
    printf("Check Max for int: %d\n",checkMax(i,j));
    printf("Check Max for float: %f\n",checkMax(l,m));
    printf("Check Max for long: %ld\n",checkMax(o,p));
    
    printf("Check with MaxINT: %d\n",checkMaxINT(i,j));
+   
+    int arr[100];
+    for(int i=0;i<100;i++){
+        arr[i] = i+1;
+    }
+    int *ptr = arr;
+	
+	
+	printf("-------------------checkMax using #define--------------------------\n");
+    b=100;
+    printf("Return value: %d\n",callCheckMax(int,ptr,b));
+    //cout<<"checkMax#Define b="<< b <<": "<<callCheckMax(int,p,b)<<endl;
+    b=0;
+    printf("Return value: %d\n",callCheckMax(int,ptr,b));
+    //cout<<"checkMax#Define b="<< b <<": "<<callCheckMax(int,p,b)<<endl;
+
+   
    
    
    return 0;
